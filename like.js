110885f8616795656b8a12b6a5b6e860ad0bc561
matteo95-byte <fratini.matteo95@gmail.com>
2026-02-06 01:48:59 +0100

    Update like.js

--- like.js
+++ like.js
@@ -18,7 +18,7 @@ const db = getFirestore(app);
 let photos = [
   { id: "fiore_yfh2db", url: "https://res.cloudinary.com/dim73lhdw/image/upload/v1770160997/fiore_yfh2db.png", destination: "Ascoli" },
   { id: "benito_shjdqg", url: "https://res.cloudinary.com/dim73lhdw/image/upload/v1770334313/benito_shjdqg.png", destination: "Sanmarco" },
-  { id: "benito", url: "https://res.cloudinary.com/dim73lhdw/image/upload/v17703313/benito_shjdqg.png", destination: "Athene" }
+  { id: "benito", url: "https://res.cloudinary.com/dim73lhdw/image/upload/v17703313/benito_shjdqg.png", destination: "Londra" }
 ];
 
 // ---- SELEZIONI HTML ----
