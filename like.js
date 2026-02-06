eded3ca4e0205db6778ce49d4afbcfaef6267043
matteo95-byte <fratini.matteo95@gmail.com>
2026-02-06 01:21:18 +0100

    Update like.js

--- like.js
+++ like.js
@@ -16,8 +16,7 @@ const db = getFirestore(app);
 // ---- ARRAY FOTO ----
 const photos = [
   { id: "fiore_yfh2db", url: "https://res.cloudinary.com/dim73lhdw/image/upload/v1770160997/fiore_yfh2db.png", destination: "Ascoli" },
-  { id: "benito_shjdqg", url: "https://res.cloudinary.com/dim73lhdw/image/upload/v1770334313/benito_shjdqg.png", destination: "Benito" },
-  { id: "montagna_cd456", url: "https://res.cloudinary.com/dim73lhdw/image/upload/v1770160997/montagna_cd456.png", destination: "Dolomiti" }
+  { id: "benito_shjdqg", url: "https://res.cloudinary.com/dim73lhdw/image/upload/v1770334313/benito_shjdqg.png", destination: "Benito" }
 ];
 
 // ---- CREAZIONE FILTRI DINAMICI ----
